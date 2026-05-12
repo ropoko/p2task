07a97516f71707d1206d38fad095a23c35cd30c1
ropoko <rodrigostramantinoli@gmail.com>
2026-05-12 18:32:49 -0300

    feat: initial commit

---
+++ src/main/index.ts
@@ -0,0 +1,58 @@
+import { app, shell, BrowserWindow } from 'electron';
+import { join } from 'path';
+import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+import icon from '../../resources/icon.png?asset';
+
+function createWindow(): void {
+	const mainWindow = new BrowserWindow({
+		width: 900,
+		height: 670,
+		show: false,
+		autoHideMenuBar: true,
+		...(process.platform === 'linux' ? { icon } : {}),
+		webPreferences: {
+			preload: join(__dirname, '../preload/index.js'),
+			sandbox: false
+		}
+	});
+
+	mainWindow.on('ready-to-show', () => {
+		mainWindow.show();
+	});
+
+	mainWindow.webContents.setWindowOpenHandler((details) => {
+		shell.openExternal(details.url);
+		return { action: 'deny' };
+	});
+
+	if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+		mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
+	} else {
+		mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
+	}
+}
+
+app.whenReady().then(() => {
+	electronApp.setAppUserModelId('com.electron');
+
+	app.on('browser-window-created', (_, window) => {
+		optimizer.watchWindowShortcuts(window);
+	});
+
+	createWindow();
+
+	app.on('activate', function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
